10529f998c219a44541e518fbcbfc77303f42b17
MaximSavhenko <uspmsm@gmail.com>
2020-08-14 18:35:29 +0300

    add slider and adaptive

--- js/main.js
+++ js/main.js
@@ -1,52 +1,15 @@
 $(document).ready(function(){
-	$(function() {  
-		$('#up').click(function() {  
-		  $('body,html').animate({scrollTop:0},1500);  
-		  return false;  
-		})  
-	  })  
-
-	$(window).on("scroll", function() {
-		// $("header").toggleClass("active", $(this).scrollTop() > $(window).height());
-		$("header").toggleClass("active", $(this).scrollTop() > 0);
-	});
-
-	$(function() {
-
-		let filtr = $("[data-filtr]");
-		filtr.on("click", function(event){
-			event.preventDefault();
-			let cat = $(this).data('filtr');
-			if(cat == 'All') {
-				$("[data-cat]").removeClass("hide");
-			} else {
-				$("[data-cat]").each(function(){
-					let workCat = $(this).data('cat');
-					if(workCat != cat) {
-						$(this).addClass('hide')
-					} else {
-						$(this).removeClass('hide')
-					}
-				});
-			}
-		});
-		$(".projects__tabs .projects__tabs-link").click(function() {
-			$(".projects__tabs .projects__tabs-link").removeClass("active").eq($(this).index()).addClass("active");
-		}).eq(0).addClass("active");
-	});
 	
-	$('.blogs__main-wrap').slick({
+	$('.foryou__slider , .bestsellers__slider').slick({
 		arrows: true,
-		dots: true,
-		slidesToShow: 3,
-		slidesToScroll: 3,
+		slidesToShow: 4,
+		slidesToScroll: 4,
 		responsive: [
 			{
 			  breakpoint: 1024,
 			  settings: {
 				slidesToShow: 2,
-				slidesToScroll: 2,
-				dots: true
+				slidesToScroll: 2
 			  }
 			},
 			{
@@ -54,13 +17,11 @@ $(document).ready(function(){
 			  settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1,
-				dots: true
 			  }
 			},
 			{
 			  breakpoint: 480,
 			  settings: {
-				arrows: false,
 				slidesToShow: 1,
 				slidesToScroll: 1
 			  }
@@ -69,14 +30,17 @@ $(document).ready(function(){
 	});
 });
 
-function openbox(box){
-	display = document.getElementById(box).style.display;
-	if(display=='none'){
-		  document.getElementById(box).style.display='flex';
-	}else{
-		  document.getElementById(box).style.display='none';
-	}
-}
+
+// function toggleMobileMenu() {
+// 	var item = document.querySelector('.foryou__slider-items'),
+// 	var slickSlide = document.querySelector('.slick-slider'),
+// 	var slickActive = document.querySelector('.slick-active');
+// 	if (item = slickActive) {
+// 		item.onclick = function () {
+			
+// 		};
+// 	}
+// };
 
 
 function toggleMobileMenu() {
@@ -91,9 +55,3 @@ function toggleMobileMenu() {
 };
 
 toggleMobileMenu();
-
-
-  
-	
-  
-
